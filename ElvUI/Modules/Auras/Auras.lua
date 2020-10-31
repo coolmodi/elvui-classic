7cdc5d264087c46eb2ca65630e5101503afa18d7
Azilroka <darktemplar9900@yahoo.com>
2020-10-31 18:17:12 -0500

    Font Issue on Auras

--- ElvUI/Modules/Auras/Auras.lua
+++ ElvUI/Modules/Auras/Auras.lua
@@ -110,10 +110,12 @@ function A:CreateIcon(button)
 	button.texture:SetInside()
 	button.texture:SetTexCoord(unpack(E.TexCoords))
 
-	button.count = button:CreateFontString(nil, 'OVERLAY', nil, 'NumberFontNormalSmall')
+	button.count = button:CreateFontString(nil, 'OVERLAY')
+	button.count:SetFontObject('NumberFontNormal')
 	button.count:SetText('\10')
 
-	button.text = button:CreateFontString(nil, 'OVERLAY', nil, 'NumberFontNormalSmall')
+	button.text = button:CreateFontString(nil, 'OVERLAY')
+	button.text:SetFontObject('NumberFontNormal')
 	button.text:SetText('\10')
 
 	button.highlight = button:CreateTexture(nil, 'HIGHLIGHT')
