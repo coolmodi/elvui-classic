10a6d8cbecd3a32911f0ca5b20c7fd0142312066
Azilroka <darktemplar9900@yahoo.com>
2021-04-20 20:03:42 -0500

    @Simpy

--- ElvUI/Core/PixelPerfect.lua
+++ ElvUI/Core/PixelPerfect.lua
@@ -30,7 +30,7 @@ function E:UIScale(init)
 	-- `init` will be the `event` if its triggered after combat
 	if init == true then -- E.OnInitialize
 		--Set variables for pixel scaling
-		local pixel, ratio = 1, 768 / E.screenheight
+		local pixel, ratio = 1, min(768 / E.screenheight, 1)
 		E.mult = (pixel / scale) - ((pixel - ratio) / scale)
 		E.Spacing = (E.PixelMode and 0) or E.mult
 		E.Border = ((not E.twoPixelsPlease) and E.PixelMode and E.mult) or E.mult*2
